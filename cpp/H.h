d95f1f4c1a42bf4b710573b764b75ab61bcdba62
Borodin <servikb1@gmail.com>
2025-01-22 11:26:52 +0700

    Add Header

--- cpp/H.h
+++ cpp/H.h
@@ -1,1 +1,19 @@
 #pragma once
+/// <summary>
+/// ”станавливаем 1 цвет консоли
+/// https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+/// </summary>
+/// <param name="color">÷вет</param>
+void setColor(int color);
+/// <summary>
+/// ”станавливаем 2 цвета консоли, дл€ фона и дл€ символа
+/// </summary>
+/// <param name="color1">÷вет1</param>
+/// <param name="color2">÷вет2</param>
+void setColor(int color1, int color2);
+/// <summary>/// —брасываем все цвета консоли на стандартный/// </summary>
+void resetColor();
+/// <summary> ”станавливаем курсор в нужную позицию </summary>
+/// <param name="x"></param>
+/// <param name="y"></param>
+void setCursorPosition(int x, int y);
